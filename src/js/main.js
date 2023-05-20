b7077e217b24df8095e5d0cd4113f736642c37ee
Milosz <miloszloniewski02@gmail.com>
2023-05-20 12:47:57 +0200

    finished challenge

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,26 @@ const burgerBtn = document.querySelector('.nav__burger-btn');
 const navItems = document.querySelector('.nav__items');
 const allNavItems = navItems.querySelectorAll('.nav__items-item');
 const allSections = document.querySelectorAll('.planet');
+const planetBtns = document.querySelectorAll('.planet__btns-panel-btn');
+const mobilePlanetBtns = document.querySelectorAll('.mobile-btns-panel__btn');
+const planetText = document.querySelectorAll('.planet__info-text');
+const planetSource = document.querySelectorAll('.planet__info-source a');
 const rotation = document.querySelector('.rotation');
 const revolution = document.querySelector('.revolution');
 const radius = document.querySelector('.radius');
 const temperature = document.querySelector('.temp');
 const URL = 'data.json';
+
+const planetOffsets = {
+	'nav__items-item--mercury': '0%',
+	'nav__items-item--venus': '200%',
+	'nav__items-item--earth': '400%',
+	'nav__items-item--mars': '600%',
+	'nav__items-item--jupiter': '800%',
+	'nav__items-item--saturn': '1000%',
+	'nav__items-item--uranus': '1200%',
+	'nav__items-item--neptune': '1400%',
+};
 
 const handleNav = () => {
 	if (!burgerBtn.classList.contains('nav__burger-btn--active')) {
@@ -26,18 +41,23 @@ const handleNav = () => {
 	);
 };
 
-gsap.to('.hero-bg', {
-	x: '-300vw',
-	repeat: -1,
-	ease: 'linear',
-	duration: 180,
-});
-
 async function handleData() {
 	const response = await axios.get(URL);
+	const data = response.data;
 	try {
-		console.log(response.data);
-		createSection(response.data);
+		const firstImg = document.querySelectorAll('.planet__imgs-img--first');
+		const secondImg = document.querySelectorAll('.planet__imgs-img--second');
+		const popupImg = document.querySelectorAll('.planet__imgs-img--popup');
+		const planetTitle = document.querySelectorAll('.planet__info-title');
+
+		for (let i = 0; i < data.length; i++) {
+			firstImg[i].setAttribute('src', `${data[i].images.planet}`);
+			secondImg[i].setAttribute('src', `${data[i].images.internal}`);
+			popupImg[i].setAttribute('src', `${data[i].images.geology}`);
+			planetTitle[i].textContent = data[i].name;
+			planetText[i].textContent = data[i].overview.content;
+			planetSource[i].setAttribute('href', `${data[i].overview.source}`);
+		}
 	} catch (error) {
 		console.error(error);
 	}
@@ -63,7 +83,88 @@ async function handleBottomInfo() {
 	}
 }
 
-handleBottomInfo();
+async function handlePlanetInfo(btn, func) {
+	const response = await axios.get(URL);
+	const data = response.data;
+	try {
+		for (let i = 0; i < data.length; i++) {
+			if (
+				allNavItems[i].classList.contains('nav__items-item--active') &&
+				allNavItems[i].classList.contains(`nav__items-item--${data[i].name.toLowerCase()}`)
+			) {
+				const btns = document.querySelectorAll(`.planet__btns-panel--${data[i].name.toLowerCase()} button`);
+				const mobileBtns = document.querySelectorAll(`.mobile-btns-panel--${data[i].name.toLowerCase()} button`);
+				if (btn.classList.contains('planet__btns-panel-btn')) {
+					btns.forEach(btn => {
+						btn.classList.remove('planet__btns-panel-btn--active');
+					});
+					btn.classList.add('planet__btns-panel-btn--active');
+				} else if (btn.classList.contains('mobile-btns-panel__btn')) {
+					mobileBtns.forEach(btn => {
+						btn.classList.remove('mobile-btns-panel__btn--active');
+					});
+					btn.classList.add('mobile-btns-panel__btn--active');
+				}
+				let content, source;
+
+				if (btn.classList.contains('overview-btn')) {
+					gsap.to(`.first-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 1,
+					});
+					gsap.to(`.second-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 0,
+					});
+					gsap.to(`.popup-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 0,
+						y: '0%',
+					});
+					content = data[i].overview.content;
+					source = data[i].overview.source;
+				} else if (btn.classList.contains('structure-btn')) {
+					gsap.to(`.second-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 1,
+					});
+					gsap.to(`.first-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 0,
+					});
+					gsap.to(`.popup-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 0,
+						y: '0%',
+					});
+					content = data[i].structure.content;
+					source = data[i].structure.source;
+				} else if (btn.classList.contains('geology-btn')) {
+					gsap.to(`.second-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 0,
+					});
+					gsap.to(`.first-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 1,
+					});
+					gsap.to(`.popup-${data[i].name.toLowerCase()}`, {
+						duration: 0.5,
+						opacity: 1,
+						y: '85%',
+					});
+					content = data[i].geology.content;
+					source = data[i].geology.source;
+				}
+
+				planetText[i].textContent = content;
+				planetSource[i].setAttribute('href', source);
+			}
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
 
 const slide = w => {
 	gsap.to('.planet__main , .footer , .mobile-btns-panel', {
@@ -83,76 +184,6 @@ const slide = w => {
 	});
 };
 
-const createSection = data => {
-	const arr = Array.from(allSections);
-	for (let i = 0; i < data.length; i++) {
-		const newPlanet = document.createElement('div');
-		newPlanet.classList.add('wrapper');
-		newPlanet.innerHTML = `
-		<div class="mobile-btns-panel">
-			<button class="mobile-btns-panel__btn">overview</button>
-			<button class="mobile-btns-panel__btn">structure</button>
-			<button class="mobile-btns-panel__btn">surface</button>
-			<div class="border"></div>
-		</div>
-
-		<div class="planet__imgs planet__imgs--${data[i].name.toLowerCase()}">
-			<img class="planet__imgs-img planet__imgs-img--first" src="${data[i].images.planet}"
-				alt="">
-			<img class="planet__imgs-img planet__imgs-img--second"
-				src="${data[i].images.internal}" alt="">
-			<img class="planet__imgs-img planet__imgs-img--popup" src="${data[i].images.geology}"
-				alt="">
-		</div>
-
-		<div class="planet__main">
-			<div class="planet__info">
-				<h2 class="planet__info-title">${data[i].name}</h2>
-				<p class="planet__info-text">${data[i].overview.content}</p>
-				<p class="planet__info-source">Source: <a href="${data[i].overview.source}"
-						target="_blank" rel="noopener noreferrer">Wikipedia</a> <img
-						src="./dist/img/icons/icon-source.svg" alt=""></p>
-			</div>
-			<div class="planet__btns-panel">
-				<button class="planet__btns-panel-btn planet__btns-panel-btn--active"><span>01</span>overview</button>
-				<button class="planet__btns-panel-btn"><span>02</span>Internal structure</button>
-				<button class="planet__btns-panel-btn"><span>03</span>surface geology</button>
-			</div>
-		</div>
-	`;
-		arr[i].appendChild(newPlanet);
-	}
-};
-
-handleData();
-
-burgerBtn.addEventListener('click', handleNav);
-allNavItems.forEach(item => {
-	item.addEventListener('click', e => {
-		handleBottomInfo();
-		allNavItems.forEach(item => item.classList.remove('nav__items-item--active'));
-		item.classList.add('nav__items-item--active');
-		const target = e.target;
-		if (target.classList.contains('nav__items-item--mercury')) {
-			slide('0%');
-		} else if (target.classList.contains('nav__items-item--venus')) {
-			slide('200%');
-		} else if (target.classList.contains('nav__items-item--earth')) {
-			slide('400%');
-		} else if (target.classList.contains('nav__items-item--mars')) {
-			slide('600%');
-		} else if (target.classList.contains('nav__items-item--jupiter')) {
-			slide('800%');
-		} else if (target.classList.contains('nav__items-item--saturn')) {
-			slide('1000%');
-		} else if (target.classList.contains('nav__items-item--uranus')) {
-			slide('1200%');
-		} else if (target.classList.contains('nav__items-item--neptune')) {
-			slide('1400%');
-		}
-	});
-});
-
 const showBurgerBtnClose = () => {
 	gsap.to('.nav__burger-btn-bars', { duration: 0.2, bottom: 0 });
 	gsap.to('.nav__burger-btn-bars--middle', { duration: 0.1, opacity: '0', bottom: 0 });
@@ -167,3 +198,49 @@ const hideBurgerBtnClose = () => {
 	gsap.to('.nav__burger-btn-bars--middle', { duration: 0.2, delay: 0.3, bottom: '7px' });
 	gsap.to('.nav__burger-btn-bars--top', { duration: 0.2, delay: 0.3, bottom: '14px' });
 };
+
+burgerBtn.addEventListener('click', handleNav);
+
+allNavItems.forEach(item => {
+	item.addEventListener('click', e => {
+		handleBottomInfo();
+		allNavItems.forEach(item => item.classList.remove('nav__items-item--active'));
+		item.classList.add('nav__items-item--active');
+		const target = e.currentTarget;
+		const classList = target.classList;
+		for (const className of classList) {
+			const offset = planetOffsets[className];
+			if (offset) {
+				slide(offset);
+				break;
+			}
+		}
+	});
+});
+
+planetBtns.forEach(btn => {
+	btn.addEventListener('click', e => {
+		const target = e.target.closest('button');
+		handlePlanetInfo(target);
+	});
+});
+
+mobilePlanetBtns.forEach(btn => {
+	btn.addEventListener('click', e => {
+		const target = e.target.closest('button');
+
+		handlePlanetInfo(target);
+	});
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	gsap.to('.hero-bg', {
+		x: '-300vw',
+		repeat: -1,
+		ease: 'linear',
+		duration: 180,
+	});
+
+	handleBottomInfo();
+	handleData();
+});
